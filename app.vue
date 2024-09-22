53010e761c867ccf0b3fb8a633663f86e0c49f3b
Eban <contact@eban.eu.org>
2024-09-22 18:37:24 +0200

    feat: Add a cool favicon & set the <head>

--- app.vue
+++ app.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+onMounted(() => {
+  useHead({
+    title: 'AEEA Generator',
+    meta: [
+      {
+        name: 'description',
+        content: 'A simple app to manage your trips using AEEA'
+      }
+    ]
+  })
+});
 </script>
 
 <template>
@@ -10,22 +21,22 @@
 </template>
 
 <style>
-  main {
-    display: flex;
-    flex-direction: row;
-    align-items: start;
-    justify-content: center;
-    height: 100vh;
-    width: min(1200px, 100vw);
-    margin: 0 auto;
-    gap: 50px;
-    padding: 20px;
-  }
+main {
+  display: flex;
+  flex-direction: row;
+  align-items: start;
+  justify-content: center;
+  height: 100vh;
+  width: min(1200px, 100vw);
+  margin: 0 auto;
+  gap: 50px;
+  padding: 20px;
+}
 
-  @media (max-width: 600px) {
-    main {
-      flex-direction: column;
-      gap: 20px;
-    }
+@media (max-width: 600px) {
+  main {
+    flex-direction: column;
+    gap: 20px;
   }
+}
 </style>
